639ac66d41875184627b7e02708db83b8e124d17
guofei <guofei1986@gmail.com>
2013-09-28 21:56:39 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lfti::Application.config.secret_key_base = '9eca386a12241e07cf3e223cdf4df31b67a488253f9c39f60d2a1398507f85dd3b7994c61e13305c5c131d36d54c84e7b9a881b880e42c495e46cedcd6138ca1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lfti::Application.config.session_store :cookie_store, key: '_lfti_session'
