2615dcdb60afc6828fe6c9aab468aad88edf232e
guofei <guofei1986@gmail.com>
2013-10-10 01:32:52 +0900

    thing

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -2,4 +2,8 @@ class ThingsController < ApplicationController
   def index
     @things = Thing.all
   end
+
+  def show
+    @thing = Thing.first
+  end
 end
--- spec/features/things_spec.rb
+++ spec/features/things_spec.rb
@@ -1,9 +1,27 @@
 require 'spec_helper'
 
 feature 'things' do
-  scenario 'user can see all things' do
-    thing = FactoryGirl.create(:thing)
-    visit things_path
-    expect(page).to have_content thing.name
+  context 'as a guest or user' do
+    let(:thing) { thing = FactoryGirl.create(:thing) }
+    scenario 'reads all things' do
+      thing
+      visit things_path
+      expect(page).to have_content thing.name
+    end
+
+    scenario 'reads a thing' do
+      visit thing_path(thing.id)
+    end
+
+    scenario 'share a new thing' do
+      pending "redirct to login page"
+    end
+  end
+
+  context 'as a user' do
+    scenario 'shares a new thing' do
+      pending
+    end
+    scenario 'adds a comment'
   end
 end
