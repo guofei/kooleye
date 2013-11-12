fcf493891d302952c57a9e66eb7fd96f735a07fb
guofei <guofei1986@gmail.com>
2013-11-13 01:30:18 +0900

    like model spec

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Like do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:thing) { FactoryGirl.create(:thing) }
+  let(:user) { FactoryGirl.create(:user) }
+  let(:like) { FactoryGirl.build(:like, user: user, thing: thing) }
+  subject{ like }
+
+  it { should respond_to(:duplication?) }
+
+  describe "duplication?" do
+    context "when user had liked" do
+      let!(:liked) { FactoryGirl.create(:like, user: user, thing: thing) }
+      its(:duplication?) { should be_true }
+    end
+
+    context "when user have not liked" do
+      its(:duplication?) { should be_false }
+    end
+  end
 end
