03de83a50b414a9bcc7c4da022491ec3c70864ed
guofei <guofei1986@gmail.com>
2013-11-30 17:04:24 +0900

    set static

--- app/helpers/things_helper.rb
+++ app/helpers/things_helper.rb
@@ -10,7 +10,7 @@ module ThingsHelper
   def link_to_thing_thumbnail(thing)
     link_to thing_path(thing), class: "thumbnail" do
       if thing.images.size > 0
-        image_tag thing.images.first.normal_url
+        image_tag thing.images.last.normal_url
       else
         thing.name
       end
