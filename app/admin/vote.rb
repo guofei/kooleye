a23e2f86601f65fc07245a6654b2c726f64bcf3e
guofei <guofei1986@gmail.com>
2013-12-06 22:46:53 +0900

    add vote admin

---
+++ app/admin/vote.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Vote do
+
+  
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params :thing_id, :user_id, :vote_type
+  #
+  # or
+  #
+  # permit_params do
+  #  permitted = [:permitted, :attributes]
+  #  permitted << :other if resource.something?
+  #  permitted
+  # end
+  
+end
