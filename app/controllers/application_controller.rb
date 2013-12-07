c5137b311daa868ac5260f6b7ea7191de9de2ea6
guofei <guofei1986@gmail.com>
2013-12-08 02:21:52 +0900

    fix sign after url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,8 @@ class ApplicationController < ActionController::Base
 
   def store_location
     # store last url - this is needed for post-login redirect to whatever the user last visited.
-    if (request.fullpath != "/users/sign_in" &&
-        request.fullpath != "/users/sign_up" &&
-        request.fullpath != "/users/password" &&
-        request.fullpath != "/admin" &&
-        request.fullpath != "/admin/login" &&
-        request.fullpath != "/admin/logout" &&
+    if (!(request.fullpath =~ /\/users/) &&
+        !(request.fullpath =~ /\/admin/) &&
         !request.xhr?) # don't store ajax calls
       session[:previous_url] = request.fullpath
     end
