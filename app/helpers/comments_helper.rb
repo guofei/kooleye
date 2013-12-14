9887d4d878558cae87d3003f6c80389140bccfc2
guofei <guofei1986@gmail.com>
2013-12-14 17:18:13 +0900

    fix name length

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,7 +1,7 @@
 module CommentsHelper
   def link_to_comment_user_page(comment)
     return link_to_user_page comment.user if comment.user
-    return comment.name if comment.name
+    return comment.name[0..10] if comment.name
     return "Guest"
   end
 end
