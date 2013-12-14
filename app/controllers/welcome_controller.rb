628235dc95ccf1592d58418cc3e3e5686612fd19
guofei <guofei1986@gmail.com>
2013-12-14 19:06:46 +0900

    add two new thing in welcomepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @hot_things = Thing.sort_by_hot.page params[:page]
+    ##Fixme: use activerecord
+    @hot_things = Kaminari.paginate_array(Thing.sort_by_new.limit(2) + Thing.sort_by_hot).page params[:page]
+    #@hot_things = (Thing.sort_by_new.limit(2) + Thing.sort_by_hot).page params[:page]
     @new_things = Thing.sort_by_new[0, 10]
   end
 end
