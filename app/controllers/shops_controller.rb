8ffb81849aeac176dad3caadb97b73d5cb6c3a42
guofei <guofei1986@gmail.com>
2014-02-14 21:38:45 +0900

    change shops controller

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -3,6 +3,7 @@ class ShopsController < ApplicationController
   include Shops
 
   def index
+    search_engine_keyword
     @suggestions = Thing.sort_by_hot.take(12)
     @new_comments = Comment.limit(8).includes(:thing, :user)
     @amazon_items = []
@@ -11,15 +12,33 @@ class ShopsController < ApplicationController
     @y_a_items = []
     keyword = params[:k]
     sort = params[:sort] == "price" ? "price" : "default"
-    if not keyword.blank?
-      @amazon_items = get_amazon_items keyword, sort: sort
-      @rakuten_items = get_rakuten_items keyword, sort: sort
-      @y_items = get_yahoo_shopping_items keyword, sort: sort
-      @y_a_items = get_yahoo_auction_items keyword, sort: sort
+    [search_engine_keyword, keyword].each do |k|
+      if not k.blank?
+        @amazon_items.concat(get_amazon_items k, sort: sort)
+        @rakuten_items.concat(get_rakuten_items k, sort: sort)
+        @y_items.concat(get_yahoo_shopping_items k, sort: sort)
+        @y_a_items.concat(get_yahoo_auction_items k, sort: sort)
+      end
     end
   end
 
   def show
     @suggestions = Thing.sort_by_hot.take(10)
+  end
+
+  private
+
+  def search_engine_keyword
+    begin
+      querys = CGI::parse URI.parse(request.headers[:referer]).query
+      query = querys["q"][0] if querys["q"]
+      query = querys["p"][0] if querys["p"]
+      if not params[:k].blank?
+        query = nil if params[:k].include?(query)
+      end
+    rescue
+      query = nil
+    end
+    query
   end
 end
